91cf4c60900d5ab7f1c8140b6d1450222fdd494f
redblacktree <dustinrasener@gmail.com>
2024-07-26 14:03:27 -0400

    Fix card rendering issue

--- scenes/game.py
+++ scenes/game.py
@@ -14,12 +14,11 @@ class GameScene():
 
     def draw(self):
         self.windowSurface.fill(WHITE)
-        self.dealer_hand = Hand(cards=self.game_state.dealer_hand['cards'], x=100, y=100, windowSurface=self.windowSurface, basicFont=self.basicFont)
-        self.player_hand = Hand(cards=self.game_state.player_hand['cards'], x=100, y=300, windowSurface=self.windowSurface, basicFont=self.basicFont)
+        self.dealer_hand = Hand(cards=self.game_state.dealer_hand['cards'], x=DEALER_CARDS_X, y=DEALER_CARDS_Y, windowSurface=self.windowSurface, basicFont=self.basicFont)
+        self.player_hand = Hand(cards=self.game_state.player_hand['cards'], x=PLAYER_CARDS_X, y=PLAYER_CARDS_Y, windowSurface=self.windowSurface, basicFont=self.basicFont)
         [x.draw(self.windowSurface) for x in self.buttons]
         self.dealer_hand.draw()
         self.player_hand.draw()        
 
     def handle_events(self, event):
         pass
-
